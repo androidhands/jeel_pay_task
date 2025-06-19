65f08115ed5b7d83e1fc0072f5be9c8cac35047e
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 13:44:03 +0300

    added page view in list view and try run

--- lib/features/jeel_pay/screens/jeel_pay_screen.dart
+++ lib/features/jeel_pay/screens/jeel_pay_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-    
-class JeelPayScreen extends StatelessWidget {
+import 'package:jeel_pay_task/features/jeel_pay/components/first_page_compoenet.dart';
+import 'package:jeel_pay_task/features/jeel_pay/components/second_page_component.dart';
 
-  const JeelPayScreen({ Key? key }) : super(key: key);
-  
+class JeelPayScreen extends StatelessWidget {
+  const JeelPayScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Jeel Pay'),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          PageView(
+            children: [FirstPageCompoenet(), SecondPageComponent()],
+          )
+        ],
+      ),
     );
   }
 }
