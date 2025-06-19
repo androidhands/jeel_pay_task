3622a9734079479121a0a9b0434ae85f0f2d1397
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 13:17:53 +0300

    create jeel pay material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const JeelPayApp());
 }
 
+class JeelPayApp extends StatelessWidget {
+  const JeelPayApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JeelPay',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const JeelPayScreen(),
+    );
+  }
+}
