275ec5136c1a602b6a342d3ea48915b6ab66617e
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 15:06:36 +0300

    wrapping list view with bloc provider and initialize web view controller

--- lib/features/jeel_pay/screens/jeel_pay_screen.dart
+++ lib/features/jeel_pay/screens/jeel_pay_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jeel_pay_task/features/jeel_pay/bsuiness_logic/web_view_cubit.dart';
 import 'package:jeel_pay_task/features/jeel_pay/components/first_page_compoenet.dart';
 import 'package:jeel_pay_task/features/jeel_pay/components/second_page_component.dart';
 
@@ -11,15 +13,18 @@ class JeelPayScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Jeel Pay'),
       ),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.8,
-            child: PageView(
-              children: [FirstPageCompoenet(), SecondPageComponent()],
-            ),
-          )
-        ],
+      body: BlocProvider(
+        create: (context) => WebViewCubit()..loadWebView(),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: PageView(
+                children: [FirstPageCompoenet(), SecondPageComponent()],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
