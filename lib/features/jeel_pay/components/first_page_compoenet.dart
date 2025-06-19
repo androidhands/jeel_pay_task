00c126d3a0fea665418a70da0cadeff5a0bed4a2
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 13:42:59 +0300

    adding placeholder for first and second page

--- lib/features/jeel_pay/components/first_page_compoenet.dart
+++ lib/features/jeel_pay/components/first_page_compoenet.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-    
-class FirstPageCompoenet extends StatelessWidget {
 
-  const FirstPageCompoenet({ Key? key }) : super(key: key);
-  
+class FirstPageCompoenet extends StatelessWidget {
+  const FirstPageCompoenet({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Center(
+      child: Text('First Page'),
     );
   }
 }
--- lib/features/jeel_pay/components/second_page_component.dart
+++ lib/features/jeel_pay/components/second_page_component.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-    
-class SecondPageComponent extends StatelessWidget {
 
-  const SecondPageComponent({ Key? key }) : super(key: key);
-  
+class SecondPageComponent extends StatelessWidget {
+  const SecondPageComponent({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Center(
+      child: Text('Second Page'),
     );
   }
 }
