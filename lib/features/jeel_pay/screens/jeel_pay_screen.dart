1264b873c7cdfa68aff18f2f3685b3877cf3192a
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 23:10:49 +0300

    adding dynamic height

--- lib/features/jeel_pay/screens/jeel_pay_screen.dart
+++ lib/features/jeel_pay/screens/jeel_pay_screen.dart
@@ -5,27 +5,30 @@ import 'package:jeel_pay_task/features/jeel_pay/components/first_page_compoenet.
 import 'package:jeel_pay_task/features/jeel_pay/components/second_page_component.dart';
 
 class JeelPayScreen extends StatelessWidget {
-  const JeelPayScreen({Key? key}) : super(key: key);
+  JeelPayScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final appBarHeight = AppBar().preferredSize.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Jeel Pay'),
       ),
       body: BlocProvider(
-        create: (context) => WebViewCubit()..loadWebView(),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.8,
-              child: PageView(
-                children: [FirstPageCompoenet(), SecondPageComponent()],
+          create: (context) => WebViewCubit()..loadWebView(),
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              SizedBox(
+                height: screenHeight - appBarHeight - statusBarHeight,
+                child: PageView(
+                  children: [FirstPageCompoenet(), SecondPageComponent()],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
