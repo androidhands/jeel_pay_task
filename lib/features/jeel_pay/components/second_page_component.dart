845a8ed5d57dbd52380a593ca2f3d27a43d6c184
Android Hands <abdelhamid.ahmed.abdo@gmail.com>
2025-06-19 13:25:14 +0300

    create socond page empty component

---
+++ lib/features/jeel_pay/components/second_page_component.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+    
+class SecondPageComponent extends StatelessWidget {
+
+  const SecondPageComponent({ Key? key }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
